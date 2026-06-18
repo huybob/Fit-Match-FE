0de83205fe04bdcdae80de36c2fc2aecc6772cba
HaiPhong22 <phongnhhe172271@fpt.edu.vn>
2026-06-18 20:30:00 +0700

    feat: add sidebar navigation routes

---
+++ src/app/members/page.tsx
@@ -0,0 +1,5 @@
+import { GymWorkspacePage } from "@/modules/gym-workspace/components/gym-workspace-page";
+
+export default function MembersPage() {
+  return <GymWorkspacePage activeSection="members" />;
+}
