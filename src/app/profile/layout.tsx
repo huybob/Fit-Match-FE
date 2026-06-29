f98443b8ead737d6c96404e83c379d8dd420b736
huybob <huytqhe176578@fpt.edu.vn>
2026-06-29 19:06:43 +0700

    fix(profile): allow ROLE_ADMIN to access profile page

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -2,5 +2,5 @@ import { ReactNode } from "react";
 import { AuthGuard } from "@/modules/auth/auth-guard";
 
 export default function ProfileLayout({ children }: { children: ReactNode }) {
-  return <AuthGuard roles={["ROLE_CUSTOMER"]}>{children}</AuthGuard>;
+  return <AuthGuard roles={["ROLE_CUSTOMER", "ROLE_ADMIN"]}>{children}</AuthGuard>;
 }
